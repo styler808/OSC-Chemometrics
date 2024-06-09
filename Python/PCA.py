f7ca616e2240efea28599cd38299e22f90b5a1fe
styler808 <styler@arizona.edu>
2024-06-09 15:08:49 -0700

    Update PCA.py

--- Python/PCA.py
+++ Python/PCA.py
@@ -10,7 +10,8 @@ import pandas as pd
 import numpy as np
 
 #LOAD AND VIEW DATASET
-fifteen_min = pd.read_csv('PTB7_LargeDataSet1300-1900_3.csv')
+#Change name of csv file to match desired dataset
+fifteen_min = pd.read_csv('DATASET.csv')
 fifteen_min.head()
 
 #DEFINE 
@@ -53,7 +54,7 @@ classes = ['Pristine', '30x1', '30x2', '30x3', '30x4', '30x5', '30x6', '30x7', '
 plt.legend(numpoints=1, handles=scatter.legend_elements()[0], labels=classes, markerscale = 3, frameon = False, fontsize = 30, loc = 'best')
 
 
-plt.savefig('PCA_PTB7_1300-1900.png')
+plt.savefig('DATASET.png')
 
 
 
